78e29cbb761f97a84b6e8476e96f1d4b9714fdcc
@mawar.theresa <mawar.theresa@mantisworks.id>
2024-02-27 21:53:26 +0700

    fixing display publications

--- has-law/src/components/Publications/index.js
+++ has-law/src/components/Publications/index.js
@@ -36,68 +36,66 @@ const Publications = () => {
     console.log("cek temdisplayedDatap", displayedData);
   }, [displayedData]);
 
-  return (
+  return displayedData?.length ? (
     <div className="w-100">
-      {displayedData?.length && (
-        <div className="container-fluid d-flex flex-column justify-content-between align-items-start paddingComponentRight paddingComponentLeft gap-4 py-4">
-          <div className="d-flex justify-content-center align-items-center w-100">
-            <span className="pro-bono-title">Publications</span>
-          </div>
-          <div className="d-flex flex-column gap-3 w-100">
-            <div className="d-flex flex-row flex-wrap justify-content-center align-items-start gap-3 mx-auto">
-              {displayedData?.map((e, i) => {
-                return (
-                  <div className="d-flex flex-column gap-2 publication-part">
-                    <div
-                      className="d-flex w-full publication-img"
-                      style={{ cursor: "pointer" }}
-                      onClick={() =>
-                        navigate(`/publications-detail/${e?.id}`, {
-                          state: { publicationId: e?.id },
-                        })
-                      }
-                    >
-                      <img className="img-fluid" src={baseUrl + e?.image} />
-                    </div>
-                    <div className="d-flex flex-column justify-content-start align-items-start w-100 mx-auto">
-                      <span className="publication-date">
-                        {moment(e?.createdAt).format("DD MMMM YYYY")}
-                      </span>
-                      <span className="publication-title">{e?.title}</span>
-                    </div>
+      <div className="container-fluid d-flex flex-column justify-content-between align-items-start paddingComponentRight paddingComponentLeft gap-4 py-4">
+        <div className="d-flex justify-content-center align-items-center w-100">
+          <span className="pro-bono-title">Publications</span>
+        </div>
+        <div className="d-flex flex-column gap-3 w-100">
+          <div className="d-flex flex-row flex-wrap justify-content-center align-items-start gap-3 mx-auto">
+            {displayedData?.map((e, i) => {
+              return (
+                <div className="d-flex flex-column gap-2 publication-part">
+                  <div
+                    className="d-flex w-full publication-img"
+                    style={{ cursor: "pointer" }}
+                    onClick={() =>
+                      navigate(`/publications-detail/${e?.id}`, {
+                        state: { publicationId: e?.id },
+                      })
+                    }
+                  >
+                    <img className="img-fluid" src={baseUrl + e?.image} />
                   </div>
-                );
-              })}
-            </div>
-          </div>
-          <div className="d-flex w-100 justify-content-center align-items-center">
-            <Button
-              style={{
-                color: "#F00",
-                borderColor: "#F00",
-                backgroundColor: "transparent",
-              }}
-              outline
-              onClick={() => navigate("/publications")}
-            >
-              <span
-                style={{
-                  color: "#F00",
-                  fontSize: "14px",
-                  fontStyle: "normal",
-                  fontWeight: 700,
-                  lineHeight: "18px",
-                  fontFamily: "Playfair Display",
-                }}
-              >
-                See More
-              </span>
-            </Button>
+                  <div className="d-flex flex-column justify-content-start align-items-start w-100 mx-auto">
+                    <span className="publication-date">
+                      {moment(e?.createdAt).format("DD MMMM YYYY")}
+                    </span>
+                    <span className="publication-title">{e?.title}</span>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
-      )}
+        <div className="d-flex w-100 justify-content-center align-items-center">
+          <Button
+            style={{
+              color: "#F00",
+              borderColor: "#F00",
+              backgroundColor: "transparent",
+            }}
+            outline
+            onClick={() => navigate("/publications")}
+          >
+            <span
+              style={{
+                color: "#F00",
+                fontSize: "14px",
+                fontStyle: "normal",
+                fontWeight: 700,
+                lineHeight: "18px",
+                fontFamily: "Playfair Display",
+              }}
+            >
+              See More
+            </span>
+          </Button>
+        </div>
+      </div>
     </div>
-  );
+  ) : null;
 };
 
 export default Publications;
