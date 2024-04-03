12915aec270c41f0f86755c7240e8cdee3e8fd67
@mawar.theresa <mawar.theresa@mantisworks.id>
2024-04-03 17:43:21 +0700

    fixing publication styling

--- has-law/src/components/Publications/Detail/index.js
+++ has-law/src/components/Publications/Detail/index.js
@@ -11,13 +11,22 @@ const baseUrl = process.env.REACT_APP_PUBLIC_URL;
 const PublicationsDetail = () => {
   const navigate = useNavigate();
   const [displayedData, setDisplayedData] = useState([]);
+  const [mobileData, setMobileData] = useState([]);
 
   const getDisplayedData = async () => {
     try {
       const resp = await axios.get(`${baseUrl}v1/publications/getall`);
       if (resp?.status === 200 && resp?.data?.status === "success") {
-        let temp = resp?.data?.data?.filter((e, i) => i < 4);
-        setDisplayedData(temp);
+        // let temp = resp?.data?.data?.filter((e, i) => i < 4);
+        console.log("cek resp", resp?.data?.data);
+        let elementsPerSubarray = 2;
+        let subarrays = [];
+        for (let i = 0; i < resp?.data?.data.length; i += elementsPerSubarray) {
+          subarrays.push(resp?.data?.data.slice(i, i + elementsPerSubarray));
+        }
+        console.log("cek subarrays", subarrays);
+        setDisplayedData(subarrays);
+        setMobileData(resp?.data?.data);
       } else {
         toast.error("Gagal mendappatkan data. Silahkan reload page");
       }
@@ -41,9 +50,47 @@ const PublicationsDetail = () => {
           <span className="pro-bono-title">Publications</span>
         </div>
 
-        <div className="d-flex flex-column gap-3 w-100">
+        <div className="d-none d-md-flex flex-column gap-3 w-100 align-items-center">
+          {displayedData?.map((item, i) => {
+            return (
+              <div
+                className="d-flex flex-row align-items-center gap-3 w-100"
+                style={{ maxWidth: 1155 }}
+              >
+                {item.map((e, idx) => {
+                  return (
+                    <div className="d-flex flex-column gap-2 publication-part">
+                      <div
+                        className="d-flex w-full publication-img"
+                        style={{ cursor: "pointer" }}
+                        onClick={() =>
+                          navigate(`/publications-detail/${e?.id}`, {
+                            state: { publicationId: e?.id },
+                          })
+                        }
+                      >
+                        <img
+                          className="d-flex img-fluid w-100"
+                          src={baseUrl + e?.image}
+                        />
+                      </div>
+                      <div className="d-flex flex-column justify-content-start align-items-start w-100 mx-auto">
+                        <span className="publication-date">
+                          {moment(e?.createdAt).format("DD MMMM YYYY")}
+                        </span>
+                        <span className="publication-title">{e?.title}</span>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="d-flex d-md-none flex-column gap-3 w-100">
           <div className="d-flex flex-row flex-wrap justify-content-center align-items-start gap-2 mx-auto w-100">
-            {displayedData?.map((e, i) => {
+            {mobileData?.map((e, i) => {
               return (
                 <div className="d-flex flex-column gap-2 publication-part">
                   <div
